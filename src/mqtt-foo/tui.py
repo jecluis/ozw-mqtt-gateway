b29eb9979e7ba4fe87d4bccfd6082fd28f520042
Joao Eduardo Luis <joao@wipwd.dev>
2020-08-21 18:30:20 +0000

    test: enhance TUI
    add a new command, and polish a few things.
    
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- src/mqtt-foo/tui.py
+++ src/mqtt-foo/tui.py
@@ -6,6 +6,9 @@ from prompt_toolkit.layout.containers import (
 )
 from prompt_toolkit.layout.controls import BufferControl
 from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.key_binding.bindings.focus import (
+	focus_next, focus_previous
+)
 from prompt_toolkit.widgets import (
 	Frame, HorizontalLine, SearchToolbar, TextArea
 )
@@ -56,10 +59,16 @@ class MQTTClient:
 
 
 	def on_message(self, client, userdata, msg):
-		self.buffer.insert_text(
-			f"mqtt > message > topic: {msg.topic} || {str(msg.payload)}"
-		)
+		self.buffer.insert_text(f"mqtt > message > topic: {msg.topic}")
 		self.buffer.newline()
+		payload = json.loads(msg.payload)
+		payload_str = json.dumps(payload, indent=4)
+		self.buffer.insert_text(f"msg: {payload_str}")
+		self.buffer.newline()
+		# self.buffer.insert_text(
+		# 	f"mqtt > message > topic: {msg.topic} || {str(msg.payload)}"
+		# )
+		# self.buffer.newline()
 
 	def get_random_str(self):
 		return ''.join(random.choice(string.ascii_letters) for i in range(15))
@@ -93,11 +102,18 @@ class MQTTClient:
 				'command': cmd_id,
 				'nonce': self.get_random_str()
 			}
+		elif cmd == "get-state":
+			action = {
+				'command': 18, # get state
+				'nonce': self.get_random_str()
+			}
 		else:
 			self.buffer.insert_text(f"-> unknown command '{cmd}'")
 			self.buffer.newline()
 			return
 
+		self.buffer.insert_text(f"-> cmd: {cmdstr}")
+		self.buffer.newline()
 		self.client.publish("ozw/action/request", json.dumps(action))
 
 
@@ -112,8 +128,10 @@ subscription_window = Window(
 def input_accept_handler(buffer: Buffer):
 	try:
 		cmd: str = buffer.text
-		subscription_buffer.insert_text(f"-> cmd: {cmd}")
-		subscription_buffer.newline()
+		if not cmd:
+			return
+		# subscription_buffer.insert_text(f"-> cmd: {cmd}")
+		# subscription_buffer.newline()
 		client.do_command(cmd)
 
 	except Exception as e:
@@ -137,7 +155,7 @@ search = SearchToolbar()
 input_field = TextArea(
 	height=1,
 	prompt=">>> ",
-	style="class:input-field",
+	# style="class:input-field",
 	multiline=False,
 	wrap_lines=False,
 	search_field=search,
@@ -153,11 +171,22 @@ root_container = HSplit([
 layout = Layout(root_container, focused_element=input_field)
 kb = KeyBindings()
 
-@kb.add('c-c')
 @kb.add('c-q')
+@kb.add('c-d')
 def event_exit(event):
 	client.stop()
 	event.app.exit()
+
+@kb.add('c-l')
+def event_clear(event):
+	subscription_buffer.text = ""
+
+@kb.add('c-c')
+def event_clear_input(event):
+	input_field.text = ""
+
+kb.add("tab")(focus_next)
+kb.add("s-tab")(focus_previous)
 
 
 client = MQTTClient(subscription_buffer)
